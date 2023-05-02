ff754e544156a7db59671e1706bb0c9557127ef5
Erin <k.erin.kissick@gmail.com>
2023-05-01 20:59:27 -0400

    claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Claw.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Claw.kt
@@ -1,4 +1,17 @@
 package org.firstinspires.ftc.teamcode.mechanisms
 
-class Claw {
+import com.qualcomm.robotcore.hardware.HardwareMap
+import com.qualcomm.robotcore.hardware.Servo
+
+import org.firstinspires.ftc.teamcode.utils.Robot
+
+class Claw(hardwareMap: HardwareMap, private val robot: Robot) {
+    private val claw = hardwareMap.get(Servo::class.java, "claw")
+
+    fun open() {
+        claw.position = 1.0
+    }
+    fun close() {
+        claw.position = 0.0
+    }
 }
