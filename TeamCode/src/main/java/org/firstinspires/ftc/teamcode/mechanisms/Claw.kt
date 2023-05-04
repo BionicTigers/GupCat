38648ffc738fcacca27c5921caee15d3627e4f50
Erin <k.erin.kissick@gmail.com>
2023-05-03 20:51:06 -0400

    claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Claw.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Claw.kt
@@ -1,17 +1,36 @@
 package org.firstinspires.ftc.teamcode.mechanisms
 
+import com.qualcomm.robotcore.hardware.DistanceSensor
 import com.qualcomm.robotcore.hardware.HardwareMap
 import com.qualcomm.robotcore.hardware.Servo
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
 
 import org.firstinspires.ftc.teamcode.utils.Robot
 
 class Claw(hardwareMap: HardwareMap, private val robot: Robot) {
     private val claw = hardwareMap.get(Servo::class.java, "claw")
+    private val distSensor = hardwareMap.get(DistanceSensor::class.java, "distSensor")
+
+    private var cycles = 0
+    private var dist = 0.0
 
     fun open() {
         claw.position = 1.0
     }
+
     fun close() {
         claw.position = 0.0
     }
+
+    @SuppressWarnings("WeakerAccess")
+    fun getDist(): Double {
+        cycles++
+        if (cycles == 10) {
+            dist = distSensor.getDistance(DistanceUnit.CM)
+            cycles = 0
+        }
+        return dist
+    }
+    fun coneDetected(): Boolean {return getDist() > 5}
+
 }
