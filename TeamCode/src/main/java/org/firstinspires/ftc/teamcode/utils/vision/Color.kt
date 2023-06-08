8349d0882dbaf1c90eda9c5d69c3862d1a51a086
owen <owenzewald@gmail.com>
2023-06-07 20:52:08 -0400

    lift

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/vision/Color.kt
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.utils.vision
+
+import org.opencv.core.Core
+import org.opencv.core.CvType
+import org.opencv.core.Mat
+import org.opencv.core.MatOfPoint
+import org.opencv.core.Scalar
+import org.opencv.core.Size
+import org.opencv.imgproc.Imgproc
+
+class Color(
+    private val lower: Scalar,
+    private val upper: Scalar,
+    private val minArea: Int,
+    private val maxArea: Int = Int.MAX_VALUE
+) {
+    private lateinit var contours: ArrayList<MatOfPoint>
+
+    fun getArea(input: Mat): Double {
+        var mask: Mat = Mat()
+        var temp: Mat = Mat()
+        var area: Double = 0.0
+
+        val kernel: Mat = Mat(Size(5.0, 5.0), CvType.CV_8UC1, Scalar(255.0))
+
+        Core.inRange(input, lower, upper, mask)
+
+        Imgproc.morphologyEx(mask, temp, Imgproc.MORPH_OPEN, kernel)
+
+        mask.release()
+        mask = Mat()
+        Imgproc.morphologyEx(temp, mask, Imgproc.MORPH_CLOSE, kernel)
+
+        contours = ArrayList()
+        Imgproc.findContours(
+            mask,
+            contours,
+            temp,
+            Imgproc.RETR_TREE,
+            Imgproc.CHAIN_APPROX_SIMPLE
+        )
+
+        for (contour in contours) {
+            area += Imgproc.contourArea(contour)
+        }
+
+        kernel.release()
+        mask.release()
+        temp.release()
+
+        return area
+    }
+
+    fun detect(area: Double) { area > minArea && area < maxArea }
+}
