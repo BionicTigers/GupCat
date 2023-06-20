e0493666c58adf82915f39ec3eca6aa537b65d30
ATK <54485972+ATK422@users.noreply.github.com>
2023-06-19 21:00:31 -0400

    Comment PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/PID.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/PID.kt
@@ -2,6 +2,12 @@ package org.firstinspires.ftc.teamcode.utils
 
 import com.qualcomm.robotcore.util.ElapsedTime
 
+/**
+ * Class to control errors
+ * Takes in either PIDTerms (Used for realtime editing) or kP, tI, tD
+ * Takes in pvMin and pvMax which is the minimum and maximum that the process value can reach
+ * Also takes in cvMin and cvMax which is the maximum output, usually -1 to 1
+ */
 class PID(
     val kP: Double,
     val tI: Double,
@@ -11,11 +17,12 @@ class PID(
     private val cvMin: Double,
     private val cvMax: Double)
 {
-    private val sampleTime = 20 //In Milliseconds
+    //Time between cycles, in ms
+    private val sampleTime = 20
 
     private val elapsedTime = ElapsedTime(ElapsedTime.Resolution.MILLISECONDS)
 
-    //Expose P, I, D, E, CV variables to outside the class
+    //Expose P, I, D, E, CV variables to outside the class for logging
     var p: Double = 0.0
         internal set
     var i: Double = 0.0
@@ -27,28 +34,50 @@ class PID(
     var cv: Double = 0.0
         internal set
 
+    /**
+     * Uses the PIDTerms class instead of kP, tI, tD
+     */
     constructor(terms: PIDTerms,
                 pvMin: Double,
                 pvMax: Double,
                 cvMin: Double,
                 cvMax: Double): this(terms.kP, terms.tI, terms.tD, pvMin, pvMax, cvMin, cvMax)
 
+    /**
+     * Calculate the control variable (Output)
+     */
     fun calculate(setPoint: Double, processValue: Double): Double {
         val dt = elapsedTime.seconds()
 
+        //Check if pid should calculate again
         if (dt < sampleTime / 1000) {
+            //Error in terms of processValue (percentage)
             val error = (setPoint - processValue) / (pvMax - pvMin)
 
             p = kP * error
             i += if (cv > cvMin && cv < cvMax && tI != 0.0) 1 / 60 * tI * error else 0.0
             d = kP * tD / 60 * (previousError - error)
 
+            //Multiply I by kP last to allow for real time editing
             cv = p + kP * i + d
 
+            //Prepare for next call
             previousError = error
             elapsedTime.reset()
         }
 
         return cv
+    }
+
+    /**
+     * Resets the integral and cv
+     */
+    fun reset() {
+        i = 0.0
+        cv = 0.0
+    }
+
+    fun log() {
+        println("P: $p, I: $i, D: $d, E: $previousError")
     }
 }
