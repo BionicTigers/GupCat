1729af7ba546f835ab771925976363cdec707cd3
ATK <54485972+ATK422@users.noreply.github.com>
2023-10-02 20:54:26 -0400

    Slides

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Slide.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Slide.kt
@@ -1,21 +1,34 @@
 package org.firstinspires.ftc.teamcode.mechanisms
 
+import com.qualcomm.hardware.lynx.LynxDcMotorController
 import com.qualcomm.robotcore.hardware.DcMotorEx
+import com.qualcomm.robotcore.hardware.DcMotorSimple
 import com.qualcomm.robotcore.hardware.HardwareMap
+import org.firstinspires.ftc.teamcode.utils.ControlHub
 import org.firstinspires.ftc.teamcode.utils.PID
 import org.firstinspires.ftc.teamcode.utils.PIDTerms
 
 class Slide(hardwareMap: HardwareMap) {
-    val Left = hardwareMap.get(DcMotorEx::class.java, "SlideL")
-    val Right = hardwareMap.get(DcMotorEx::class.java, "SlideR")
+    val left = hardwareMap.get(DcMotorEx::class.java, "SlideL")
+    val right = hardwareMap.get(DcMotorEx::class.java, "SlideR")
+    val pid = PID(PIDTerms(), 0.0, 1000.0, -1.0, 1.0)
+    private val hub = ControlHub(hardwareMap, hardwareMap.get("Control Hub") as LynxDcMotorController)
 
-    var height = 0
-        set(value: Int) {
-            field = value.coerceIn(0, 800)
+    var height = 0.0
+        set(value: Double) {
+            field = value.coerceIn(0.0, 800.0)
         }
 //    val pid = PID(PIDTerms(), )
+    init {
+        hub.setJunkTicks()
+        right.direction = DcMotorSimple.Direction.REVERSE
+    }
 
     fun update() {
+        val encoderTicks = hub.getEncoderTicks(0).toDouble()
+        val power = pid.calculate(height, encoderTicks)
+        left.power = power
+        right.power = power
 
     }
 }
