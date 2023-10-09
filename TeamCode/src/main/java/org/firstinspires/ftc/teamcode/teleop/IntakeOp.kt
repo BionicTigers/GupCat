ffa56a8c49399b917670356550894a890eef9848
owen <owenzewald@gmail.com>
2023-10-08 21:00:02 -0400

    main  teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/IntakeOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/IntakeOp.kt
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode.teleop
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap
 import org.firstinspires.ftc.teamcode.mechanisms.Intake
 import org.firstinspires.ftc.teamcode.utils.Robot
 import org.firstinspires.ftc.teamcode.utils.input.GamepadEx
 
+@TeleOp
 class IntakeOp : LinearOpMode() {
     override fun runOpMode() {
         val robot = Robot(this)
