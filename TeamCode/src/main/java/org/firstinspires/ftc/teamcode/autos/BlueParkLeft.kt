1a54eb26d2ccd4d8bea95663db8b826f0c4eb55a
Benjamin Beseler <107444382+AlexKaiser26@users.noreply.github.com>
2023-10-11 21:01:28 -0400

    Wrote a blue parking auto w/ variations for left and right and restructured some odometry

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/BlueParkLeft.kt
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.autos
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import org.firstinspires.ftc.teamcode.utils.Pose
+import org.firstinspires.ftc.teamcode.utils.Robot
+
+@Autonomous(name = "BlueParkLeft")
+class BlueParkLeft : LinearOpMode() {
+    override fun runOpMode() {
+        val robot = Robot(this)
+        robot.pose = Pose(300.0, 300.0, 0.0)
+        BluePark(robot, hardwareMap)
+    }
+}
