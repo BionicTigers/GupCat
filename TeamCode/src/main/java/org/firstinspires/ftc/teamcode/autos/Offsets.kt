93a9e64bf8070af199fe8cedd7edc3fb183a377c
Benjamin Beseler <107444382+AlexKaiser26@users.noreply.github.com>
2023-10-22 21:00:49 -0400

    Changed offsets to an object, made some progress on preload auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/Offsets.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/Offsets.kt
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.autos
 
 import org.firstinspires.ftc.teamcode.utils.Vector2
 
-class Offsets {
+object Offsets {
     private val center = Vector2(2731.0, 950.0)
     private val left = Vector2(2462.0, 835.0)
     private val right = Vector2(2993.0, 835.0)
