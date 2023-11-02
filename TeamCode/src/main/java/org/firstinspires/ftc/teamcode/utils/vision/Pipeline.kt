13e43ef2da340b99404031ecf394379e936d7b1a
ATK <54485972+ATK422@users.noreply.github.com>
2023-11-01 20:48:28 -0400

    Make opencv good again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/vision/Pipeline.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/vision/Pipeline.kt
@@ -1,63 +1,63 @@
-//package org.firstinspires.ftc.teamcode.utils.vision
-//
-//import org.firstinspires.ftc.teamcode.utils.Vector2
-//import org.opencv.core.Mat
-//import org.opencv.core.MatOfPoint
-//import org.opencv.core.Scalar
-//import org.opencv.imgproc.Imgproc
-//import org.openftc.easyopencv.OpenCvPipeline
-//
-//data class Detection(val name: String, val position: Vector2)
-//
-//class Pipeline(private val injection: OpenCv) : OpenCvPipeline() {
-//
-//    private var detection: Detection? = null
-//
-//    //This is essentially the Main method for the pipeline
-//    //On every new frame from the camera, this is ran
-//    override fun processFrame(input: Mat): Mat? {
-//        detection = null
-//
-//        val hsvMatUncropped = Mat()
-//
-//        val contours = ArrayList<MatOfPoint>()
-//
-//        //Convert the input image into HSV
-//        Imgproc.cvtColor(input, hsvMatUncropped, Imgproc.COLOR_RGB2HSV)
-//
-//        val hsvMat = if (injection.crop != null) hsvMatUncropped.submat(injection.crop) else hsvMatUncropped
-//
-//        var high : Double = 0.0
-//        var name: String? = null
-//
-//        for ((iName, color) in injection.signals){
-//            val area : Double = color.getArea(hsvMat)
-//
-////            println("$name: $area")
-//
-//            contours.addAll(color.contours)
-//
-//            //Check if the detection is in the minimum radius
-//            //Also check if its higher than the previous highest area
-//            if (color.detect(area) && high < area) {
-//                high = area
-//                name = iName
-//            }
-//        }
-//
-//        if (name != null) {
-//            detection = Detection(name, )
-//        }
-//
-//        Imgproc.drawContours(input, contours, -1, Scalar(250.0,0.0,250.0), 2)
-//
-//        //Release so we don't get any memory leaks
-//        hsvMatUncropped.release()
-//        hsvMat.release()
-//        return if (injection.crop != null) input.submat(injection.crop) else input
-//    }
-//
-//    fun getDetection(): Detection? {
-//        return detection
-//    }
-//}
+package org.firstinspires.ftc.teamcode.utils.vision
+
+import org.firstinspires.ftc.teamcode.utils.Vector2
+import org.opencv.core.Mat
+import org.opencv.core.MatOfPoint
+import org.opencv.core.Scalar
+import org.opencv.imgproc.Imgproc
+import org.openftc.easyopencv.OpenCvPipeline
+
+data class Detection(val name: String, val position: Vector2)
+
+class Pipeline(private val injection: OpenCv) : OpenCvPipeline() {
+
+    private var detection: Detection? = null
+
+    //This is essentially the Main method for the pipeline
+    //On every new frame from the camera, this is ran
+    override fun processFrame(input: Mat): Mat? {
+        detection = null
+
+        val hsvMatUncropped = Mat()
+
+        val contours = ArrayList<MatOfPoint>()
+
+        //Convert the input image into HSV
+        Imgproc.cvtColor(input, hsvMatUncropped, Imgproc.COLOR_RGB2HSV)
+
+        val hsvMat = if (injection.crop != null) hsvMatUncropped.submat(injection.crop) else hsvMatUncropped
+
+        var high : Double = 0.0
+        var detectionToReturn: Pair<String, Vector2>? = null
+
+        for ((iName, color) in injection.signals){
+            val (area, position) = color.calculate(hsvMat)
+
+//            println("$name: $area")
+
+            contours.addAll(color.contours)
+
+            //Check if the detection is in the minimum radius
+            //Also check if its higher than the previous highest area
+            if (color.detect(area) && high < area) {
+                high = area
+                detectionToReturn = Pair(iName, position)
+            }
+        }
+
+        if (detectionToReturn != null) {
+            detection = Detection(detectionToReturn.first, detectionToReturn.second)
+        }
+
+        Imgproc.drawContours(input, contours, -1, Scalar(250.0,0.0,250.0), 2)
+
+        //Release so we don't get any memory leaks
+        hsvMatUncropped.release()
+        hsvMat.release()
+        return if (injection.crop != null) input.submat(injection.crop) else input
+    }
+
+    fun getDetection(): Detection? {
+        return detection
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/vision/VisionConstants.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/vision/VisionConstants.kt
@@ -1,28 +1,28 @@
-//package org.firstinspires.ftc.teamcode.utils.vision
-//
-//import com.acmerobotics.dashboard.config.Config
-//import org.opencv.core.Scalar
-//
-//@Config
-//object VisionConstants {
-//    private val ORANGE_LOWER = Scalar(6.0, 150.0, 130.0)
-//    private val ORANGE_UPPER = Scalar(20.0, 235.0, 215.0)
-//
-//    private val GREEN_LOWER = Scalar(40.0, 100.0, 100.0)
-//    private val GREEN_UPPER = Scalar(74.0, 210.0, 220.0)
-//
-//    private val PURPLE_LOWER = Scalar(130.0, 130.0, 21.0)
-//    private val PURPLE_UPPER = Scalar(180.0, 200.0, 200.0)
-//
-//    @JvmField
-//    val ORANGE: Color = Color(ORANGE_LOWER, ORANGE_UPPER, 1000)
-//    @JvmField
-//    val GREEN: Color = Color(GREEN_LOWER, GREEN_UPPER, 1000)
-//    @JvmField
-//    val PURPLE: Color = Color(PURPLE_LOWER, PURPLE_UPPER, 1000)
-//
-//
-//    const val EXPOSURE: Long = 20
-//    const val GAIN = 0
-//    const val WHITE_BALANCE = 0
-//}
+package org.firstinspires.ftc.teamcode.utils.vision
+
+import com.acmerobotics.dashboard.config.Config
+import org.opencv.core.Scalar
+
+@Config
+object VisionConstants {
+    private val ORANGE_LOWER = Scalar(6.0, 150.0, 130.0)
+    private val ORANGE_UPPER = Scalar(20.0, 235.0, 215.0)
+
+    private val GREEN_LOWER = Scalar(40.0, 100.0, 100.0)
+    private val GREEN_UPPER = Scalar(74.0, 210.0, 220.0)
+
+    private val PURPLE_LOWER = Scalar(130.0, 130.0, 21.0)
+    private val PURPLE_UPPER = Scalar(180.0, 200.0, 200.0)
+
+    @JvmField
+    val ORANGE: Color = Color(ORANGE_LOWER, ORANGE_UPPER, 1000)
+    @JvmField
+    val GREEN: Color = Color(GREEN_LOWER, GREEN_UPPER, 1000)
+    @JvmField
+    val PURPLE: Color = Color(PURPLE_LOWER, PURPLE_UPPER, 1000)
+
+
+    const val EXPOSURE: Long = 20
+    const val GAIN = 0
+    const val WHITE_BALANCE = 0
+}
