051a317f7b4f9248e2ac39fcce3a64a2efa8f81a
Erin <k.erin.kissick@gmail.com>
2023-11-08 01:55:50 -0500

    april tags is done!!!!!!!!!! (not tested tho)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/AprilTagOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/AprilTagOp.kt
@@ -1,20 +1,40 @@
 package org.firstinspires.ftc.teamcode.teleop
-//
-//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
-//import com.qualcomm.robotcore.eventloop.opmode.TeleOp
-//import org.firstinspires.ftc.teamcode.utils.vision.AprilTags
-//import org.firstinspires.ftc.vision.apriltag.AprilTagDetection
-//
-//@TeleOp(name = "AprilTagOp")
-//class AprilTagOp : LinearOpMode() {
-//    val aprilTags = AprilTags(hardwareMap)
-//
-//    override fun runOpMode() {
-//        waitForStart()
-//
-//        while (opModeIsActive()) {
-//            aprilTags.aprilTagLog(telemetry, aprilTags.calculateRobotPos(), aprilTags.calcFinalPose(aprilTags.calculateRobotPos()))
-//            telemetry.update()
-//        }
-//    }
-//}
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.teamcode.utils.Robot
+import org.firstinspires.ftc.teamcode.utils.input.GamepadEx
+import org.firstinspires.ftc.teamcode.utils.vision.AprilTags
+
+@TeleOp(name = "AprilTagOp")
+class AprilTagOp : LinearOpMode() {
+    override fun runOpMode() {
+        val robot = Robot(this)
+        val (_, gamepad2) = robot.getGamepads()
+        val aprilTags = AprilTags(robot, hardwareMap)
+
+        gamepad2.getButton(GamepadEx.Buttons.A).onStart{
+            aprilTags.aprilTagLog(aprilTags.calculateRobotPos(), telemetry)
+        }
+
+        gamepad2.getButton(GamepadEx.Buttons.B).onStart{
+            telemetry.addLine("Apriltags in view: ${aprilTags.getAprilTagDetections()}")
+        }
+
+        gamepad2.getButton(GamepadEx.Buttons.Y).onStart{
+            aprilTags.toggleLiveView()
+        }
+
+        gamepad2.getButton(GamepadEx.Buttons.X).onStart{
+            aprilTags.toggleATProcessor()
+        }
+
+
+        waitForStart()
+
+        while (opModeIsActive()) {
+            aprilTags.calculateRobotPos()
+            telemetry.update()
+        }
+    }
+}
